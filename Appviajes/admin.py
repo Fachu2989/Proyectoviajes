53e3c54f14bca7b79c580562be06a9ac82d05d84
Fachu2989 <Fachu290589@gmail.com>
2022-10-30 21:27:54 -0300

    Update admin.py
    importando y registrando al administrador los modulos

--- Appviajes/admin.py
+++ Appviajes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Avion, Colectivo, Paquete
+
 # Register your models here.
+admin.site.register(Paquete)
+admin.site.register(Avion)
+admin.site.register(Colectivo)
+
